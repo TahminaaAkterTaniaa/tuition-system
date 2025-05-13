f94d126785f680d20681a5ff0bf5f3ab2d66e993
TahminaaAkterTaniaa <tahmina_akter_tania@nexstack.sg>
2025-05-13 23:36:15 +0600

    feat: configure PostgreSQL database connection URL and remove postinstall flag

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -371,12 +371,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://neondb_owner:npg_8azXxte1ZuFd@ep-lucky-mud-a4uzvd1h-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
       }
     }
   },
